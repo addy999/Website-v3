13f684526da8935d0da53337541c038144495432
Addy Bhatia <addy.bhatia@hey.com>
2022-07-08 13:26:21 -0400

    improve icons

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import { toast } from '@zerodevx/svelte-toast';
+    import { Icon, Mail, Calendar } from "svelte-hero-icons";
 
     const onEmail = () => {
         navigator.clipboard.writeText('addy.bhatia@hey.com');
@@ -13,12 +14,14 @@
 
 <footer id="contact">
     <div>
-        <button on:click={onEmail}><img src="./icons8-mail-100.png" alt="email"/></button>
-        <a href="http://linkedin.com/in/addybhatia" target="blank_" id="in"><img src="./icons8-linkedin-96.png" alt="linkedin"/></a>
+        <button on:click={onEmail}>
+            <Icon src="{Mail}" solid />
+        </button>
+        <a href="http://linkedin.com/in/addybhatia" target="blank_" id="in">
+            <img src="./icons8-linkedin-96.png" alt="linkedin"/>
+        </a>
         <a href="https://calendly.com/addybhatia/" target="blank_">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd" />
-              </svg>
+            <Icon src="{Calendar}" solid />
         </a>
     </div>
 </footer>
@@ -31,7 +34,7 @@
     }
 
     img {
-        filter: invert(1)
+        filter: invert(1);
     }
 
     div {
@@ -49,18 +52,10 @@
         padding: 0px;
         color: white;
     }
-
-    #in {
-        width: 27.5px;
-    }
-
+    
     @media (min-width: 600px) {
         button, a {
             width: 30px;
-        }
-
-        #in {
-            width: 32.5px; 
         }
 
         div {
